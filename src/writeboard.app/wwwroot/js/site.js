6c9c06a439e0d5e8441bd1b30a088647a8ad0b80
James Kelley <jkelley3@gmail.com>
2017-02-21 18:58:18 -0500

    Initial marker tool, layout tweaks

--- src/writeboard.app/wwwroot/js/site.js
+++ src/writeboard.app/wwwroot/js/site.js
@@ -2,3 +2,54 @@
     //initialize canvas
     $('#canvas').sketch({ defaultColor: "#ff0" });
 });
+
+var marker = "rgba(0,0,0,1)";
+var markerWidth = 1;
+
+var lastEvent;
+var mouseDown = false;
+
+var element = $('#wb-canvas');
+var context = element.getContext('2d');
+var $canvas = $('#wb-canvas');
+
+$canvas.mousedown(function (e) {
+    lastEvent = e;
+    mouseDown = true;
+    console.log(lastEvent);
+}).mousemove(function (e) {
+    if (mouseDown) {
+        context.beginPath();
+
+        context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+        context.lineTo(e.offsetX, e.offsetY);
+        context.lineWidth = markerWidth;
+        context.strokeStyle = marker;
+        context.lineCap = 'round';
+        context.stroke();
+
+        lastEvent = e;
+    }
+
+}).mouseup(function () {
+    mouseDown = false;
+});
+
+
+/****CLEAR****/
+
+var clear = function () {
+    context.clearRect(0, 0, 575, 300);
+};
+
+$('#clear').on("click", clear);
+
+/****CHANGE MARGER WIDTH****/
+
+var changeWidth = function () {
+    markerWidth = $("#marker").val();
+    console.log(markerWidth);
+};
+
+
+$("#marker").change(changeWidth);
