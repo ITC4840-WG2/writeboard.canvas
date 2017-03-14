b39ebece06de1e395743bb2cc69bc522da2591fa
kelleyj <jkelley3@gmail.com>
2017-03-14 19:32:40 -0400

    Final UI layout, initial DB connection and registration code (WRIT-37, WRIT-43)

--- src/writeboard.app/Controllers/App.cs
+++ src/writeboard.app/Controllers/App.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using System.Data;
+using System.Configuration;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Mvc;
 
 namespace writeboard.app.Controllers
 {
@@ -12,6 +16,19 @@ namespace writeboard.app.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        [HttpPost]
+        protected IActionResult RegisterWriteBoard()
+        {
+            SqlConnection conn = new SqlConnection("Server=writeboard-db.database.windows.net;Database=writeboard;User Id=wbadmin;Password=neuedu#2017");
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("INSERT INTO writeboards VALUES(@key, @email, @desc, NULL);", conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return View("Index");
         }
     }
 }
--- src/writeboard.app/wwwroot/js/site.js
+++ src/writeboard.app/wwwroot/js/site.js
@@ -23,8 +23,23 @@ $(function () {
     });
 
     //canvas
-    var canvas = $('#wb-canvas')[0].getContext('2d');
+    var canvas = $('#wb-canvas')[0];
+    var context = canvas.getContext('2d');
 
+    //mini map
+    var map = $('#wb-map')[0];
+    var mapContext = map.getContext('2d');
+    //drawMap(canvas, mapContext, 1920, 1080);
+
+    //function drawMap(canvas, mapContext, width, height) {
+    //    var image = new Image();
+    //    image.src = canvas.toDataURL();
+    //    mapContext.drawImage(image, 0, 0, width, height);
+
+    //    setTimeout(drawMap, 10, canvas, mapContext, width, height);
+    //}
+
+    //drawing events
     var lastEvent;
     var mouseDown = false;
     $('#wb-canvas').mousedown(function (e) {
@@ -32,20 +47,68 @@ $(function () {
         mouseDown = true;
     }).mousemove(function (e) {
         if (mouseDown) {
-            canvas.beginPath();
-            canvas.globalCompositeOperation = tool.globalCompositeOperation;
-            canvas.moveTo(lastEvent.offsetX, lastEvent.offsetY);
-            canvas.lineTo(e.offsetX, e.offsetY);
-            canvas.lineWidth = lineWidth;
-            canvas.strokeStyle = tools[tool].color;
-            canvas.lineCap = 'round';
-            canvas.stroke();
+            context.beginPath();
+            context.globalCompositeOperation = tool.globalCompositeOperation;
+            context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+            context.lineTo(e.offsetX, e.offsetY);
+            context.lineWidth = lineWidth;
+            context.strokeStyle = tools[tool].color;
+            context.lineCap = 'round';
+            context.stroke();
 
             lastEvent = e;
         }
     }).mouseup(function () {
         mouseDown = false;
     });
+
+    //whiteboard capture
+    $('#wb-capture').css('top', $('#wb-cam').position().top);
+    $('#wb-capture').css('left', $('#wb-cam').position().left);
+    $(window).resize(function () {
+        $('#wb-capture').css('top', $('#wb-cam').position().top);
+        $('#wb-capture').css('left', $('#wb-cam').position().left);
+    });
+    var cam = $('#wb-cam')[0];
+    $('#wb-capture').click(function () {
+        
+        $('#wb-cam').data('enabled', 'true');
+        $('#wb-map').attr('visibility', 'hidden');
+        $('#wb-cam').attr('visibility', 'visible');
+        $('#wb-capture').html('<i class="fa fa-close"></i>');
+
+        //$('#wb-cam').data('enabled', 'false');
+        //$('#wb-map').attr('visibility', 'visible');
+        //$('#wb-cam').attr('visibility', 'hidden');
+        //$('#wb-capture').html('<i class="fa fa-camera"></i>');
+
+        var videoURL = window.URL || window.webkitURL;
+
+        navigator.getMedia = navigator.getUserMedia ||
+                             navigator.webkitGetUserMedia ||
+                             navigator.mozGetUserMedia ||
+                             navigator.msGetUserMedia;
+        navigator.getMedia({
+            video: true,
+            audio: false
+        }, function (stream) {
+            cam.src = videoURL.createObjectURL(stream);
+            cam.play();
+        }, function (error) {
+            alert("WriteBoard Camera Error");
+        });
+    });
+    
+    //overlay image on canvas and map live
+    cam.addEventListener('play', function () {
+        //overlayCapture(this, context, 1920, 1080);
+    });
+
+    function overlayCapture(capture, context, width, height) {
+        context.drawImage(capture, 0, 0, width, height);
+
+        setTimeout(overlayCapture, 10, cam, context, width, height);
+    }
 
     //eraser button
     $('#wb-eraser').click(function () {
@@ -59,7 +122,7 @@ $(function () {
 
     //clear button
     $('#wb-clear').click(function () {
-        canvas.clearRect(0, 0, 800, 400);
+        context.clearRect(0, 0, 1920, 1080);
     });
 
     //marker width
